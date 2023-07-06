b044d0603bec6258bb76b62467e79c1f546583a5
Mateja <markovic.mateja@hotmail.com>
2023-07-06 11:19:05 +0200

    Neue Methoden erstellt. Seiten wechseln. Arraylists erstellen. Tasks speichern.

--- app/src/main/java/com/example/whattodo/AddList.java
+++ app/src/main/java/com/example/whattodo/AddList.java
@@ -2,13 +2,51 @@ package com.example.whattodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AddList extends AppCompatActivity {
+
+    Button newTask;
+    Button hinzufuegen;
+
+    EditText taskField;
+    String task;
+    ArrayList<String> tasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_list);
+
+        taskField = findViewById(R.id.task);
+        tasks = new ArrayList<>();
+
+        newTask = findViewById(R.id.newTask);
+        hinzufuegen = findViewById(R.id.hinzufuegen);
+
+        newTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task = taskField.getText().toString();
+                tasks.add(task);
+                taskField.getText().clear();
+            }
+        });
+
+        hinzufuegen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddList.this, MainActivity.class);
+                intent.putStringArrayListExtra("Tasklist", tasks);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/whattodo/MainActivity.java
+++ app/src/main/java/com/example/whattodo/MainActivity.java
@@ -2,9 +2,21 @@ package com.example.whattodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button newList;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +24,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+        newList = findViewById(R.id.button);
+        textView = findViewById(R.id.taskList);
+
+        newList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddList.class);
+                startActivity(intent);
+            }
+        });
+
+        Intent intent = getIntent();
+        ArrayList<String> tasks = intent.getStringArrayListExtra("Tasklist");
+
+        if (tasks != null) {
+            StringBuilder buildList = new StringBuilder();
+
+            for (String task : tasks) {
+                buildList.append(task).append("\n"+"\n");
+            }
+
+            String showList = buildList.toString();
+
+            textView.setText(showList);
+        }
     }
 }
